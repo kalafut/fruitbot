17efc9c97d15d9173228e48691d074cc490a75a5
Jim Kalafut <jim@kalafut.net>
2012-05-27 19:23:27 -0700

    jslint cleanup

--- mybot.js
+++ mybot.js
@@ -1,10 +1,195 @@
-var ns = new function() {
+/*global WIDTH, HEIGHT, get_number_of_item_types, get_my_x, get_my_y, get_opponent_x, get_opponent_y, EAST, NORTH, WEST, SOUTH, TAKE, PASS */
+/*global get_board, get_total_item_count, get_my_item_count, get_opponent_item_count */
 
-    var MY = 1, OPP = 2;
-    var num_cells;
-    var num_types;
-    var x_delta = new Array();
-    var y_delta = new Array();
+var ns = (function () {
+    "use strict";
+    var MY, OPP, num_cells, num_types, x_delta, y_delta, pass, Board;
+
+    Board = {
+        init: function (start_board) {
+            var i, j, x, y;
+
+            // initialize board
+            Board.board = [];
+
+            for (i = 0; i < WIDTH; i += 1) {
+                Board.board[i] = [];
+                for (j = 0; j < HEIGHT; j += 1) {
+                    Board.board[i][j] = start_board[i][j];
+                }
+            }
+
+            Board.history = [];
+
+            Board.oppCollected = [];
+            Board.myCollected = [];
+            for (i = 0; i < get_number_of_item_types(); i += 1) {
+                Board.myCollected[i] = 0;
+                Board.oppCollected[i] = 0;
+            }
+
+            Board.myX = get_my_x();
+            Board.myY = get_my_y();
+            Board.oppX = get_opponent_x();
+            Board.oppY = get_opponent_y();
+        },
+        processMove: function (myMove, oppMove) {
+            var undo = {
+                myX: Board.myX,
+                myY: Board.myY,
+                oppX: Board.oppX,
+                oppY: Board.oppY
+            };
+            Board.history.push(undo);
+
+            if ((Board.myX === Board.oppX) && (Board.myY === Board.oppY) && (myMove === TAKE) && (oppMove === TAKE) && Board.board[Board.myX][Board.myY] > 0) {
+                undo.myFruitType = Board.board[Board.myX][Board.myY];
+                undo.myFruitX = Board.myX;
+                undo.myFruitY = Board.myY;
+                undo.myFruitCnt = 0.5;
+
+                undo.oppFruitType = Board.board[Board.oppX][Board.oppY];
+                undo.oppFruitX = Board.oppX;
+                undo.oppFruitY = Board.oppY;
+                undo.oppFruitCnt = 0.5;
+
+                Board.myCollected[Board.board[Board.myX][Board.myY] - 1] = Board.myCollected[Board.board[Board.myX][Board.myY] - 1] + 0.5;
+                Board.oppCollected[Board.board[Board.oppX][Board.oppY] - 1] = Board.oppCollected[Board.board[Board.oppX][Board.oppY] - 1] + 0.5;
+                Board.board[Board.myX][Board.myY] = 0;
+
+            } else {
+                if (myMove === TAKE && Board.board[Board.myX][Board.myY] > 0) {
+                    undo.myFruitType = Board.board[Board.myX][Board.myY];
+                    undo.myFruitX = Board.myX;
+                    undo.myFruitY = Board.myY;
+                    undo.myFruitCnt = 1;
+
+                    Board.myCollected[Board.board[Board.myX][Board.myY] - 1] += 1;
+                    Board.board[Board.myX][Board.myY] = 0;
+                }
+                if (oppMove === TAKE && Board.board[Board.oppX][Board.oppY] > 0) {
+                    undo.oppFruitType = Board.board[Board.oppX][Board.oppY];
+                    undo.oppFruitX = Board.oppX;
+                    undo.oppFruitY = Board.oppY;
+                    undo.oppFruitCnt = 0.5;
+
+                    Board.oppCollected[Board.board[Board.oppX][Board.oppY] - 1] += 1;
+                    Board.board[Board.oppX][Board.oppY] = 0;
+                }
+            }
+            if (myMove === NORTH) {
+                if (Board.myY - 1 >= 0) {
+                    Board.myY = Board.myY - 1;
+                }
+            }
+            if (oppMove === NORTH) {
+                if (Board.oppY - 1 >= 0) {
+                    Board.oppY = Board.oppY - 1;
+                }
+            }
+            if (myMove === SOUTH) {
+                if (Board.myY + 1 < HEIGHT) {
+                    Board.myY = Board.myY + 1;
+                }
+            }
+            if (oppMove === SOUTH) {
+                if (Board.oppY + 1 < HEIGHT) {
+                    Board.oppY = Board.oppY + 1;
+                }
+            }
+            if (myMove === EAST) {
+                if (Board.myX + 1 < WIDTH) {
+                    Board.myX = Board.myX + 1;
+                }
+            }
+            if (oppMove === EAST) {
+                if (Board.oppX + 1 < WIDTH) {
+                    Board.oppX = Board.oppX + 1;
+                }
+            }
+            if (myMove === WEST) {
+                if (Board.myX - 1 >= 0) {
+                    Board.myX = Board.myX - 1;
+                }
+            }
+            if (oppMove === WEST) {
+                if (Board.oppX - 1 >= 0) {
+                    Board.oppX = Board.oppX - 1;
+                }
+            }
+
+            if (Board.myX === Board.oppX && Board.myY === Board.oppY) {
+                Board.history[Board.myX][Board.myY] = 3;
+            } else {
+                Board.history[Board.myX][Board.myY] = 1;
+                Board.history[Board.oppX][Board.oppY] = 2;
+            }
+
+
+        },
+        undoMove: function () {
+            var undo = Board.history.pop();
+
+            Board.myX = undo.myX;
+            Board.myY = undo.myY;
+            if (undo.myFruitType !== undefined) {
+                Board.myCollected[undo.myFruitType - 1] -= undo.myFruitCnt;
+                Board.board[undo.myX][undo.myY] = undo.myFruitType;
+            }
+
+            Board.oppX = undo.oppX;
+            Board.oppY = undo.oppY;
+            if (undo.oppFruitType !== undefined) {
+                Board.oppCollected[undo.oppFruitType - 1] -= undo.oppFruitCnt;
+                Board.board[undo.oppX][undo.oppY] = undo.oppFruitType;
+            }
+        },
+
+        noMoreItems: function () {
+            var i, j;
+            for (i = 0; i < WIDTH; i += 1) {
+                for (j = 0; j < HEIGHT; j += 1) {
+                    if (Board.board[i][j] !== 0) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        },
+        movegen: function () {
+            var moves, gen;
+
+            gen = function (x, y) {
+                var moves = [PASS]; // PASS is probably a waste of search time
+                if (x >= 1) {
+                    moves.push(WEST);
+                }
+                if (x <= WIDTH - 2) {
+                    moves.push(EAST);
+                }
+                if (y >= 1) {
+                    moves.push(NORTH);
+                }
+                if (y <= HEIGHT - 2) {
+                    moves.push(SOUTH);
+                }
+                if (Board.board[x][y] > 0) {
+                    moves.push(TAKE);
+                }
+                return moves;
+            };
+
+            moves = {
+                myMoves: gen(Board.myX, Board.myY),
+                oppMoves: gen(Board.oppX, Board.oppY)
+            };
+        }
+    };
+
+    MY = 1;
+    OPP = 2;
+    x_delta = [];
+    y_delta = [];
 
     x_delta[EAST] = 1;
     x_delta[WEST] = -1;
@@ -20,75 +205,22 @@ var ns = new function() {
     y_delta[TAKE] = 0;
     y_delta[PASS] = 0;
 
-    var std_moves = new Array( EAST, WEST, NORTH, SOUTH, TAKE, PASS );
 
-
-    var pass = true;
-
-    this.new_game = function() {
-        num_cells = WIDTH * HEIGHT;
-        num_types = get_number_of_item_types();
-    }
-
-    this.make_move = function() {
-        var board = get_board();
-        var min_dist = 9e99;
-        var best_move;
-
-        if( pass ) {
-            pass = false;
-            //return PASS;
-        }
-
-        //score(board);
-
-
-        var fruit_goal=new Array();
-        for(var i = 1; i <= get_number_of_item_types(); i++) {
-            
-            fruit_goal.push( { fruit:i, count:get_total_item_count(i) } );
-        }
-        fruit_goal.sort( function(a,b) { return b.count - a.count; } );
-
-        board = reduce_board(board);
-
-
-        return gravity(board);
-    }
-
-
-    function search() {
-        var my_inv_moves = board.get_invalid_moves(MY);
-        var opp_inv_moves = board.get_invalid_moves(OPP);
-
-        var my_move,opp_move;
-
-        for( my_move in std_moves ) {
-            if( my_inv_moves.indexOf( my_move ) != -1 ) {
-                continue;
-            }
-            for( opp_move in std_moves ) {
-                if( opp_inv_moves.indexOf( opp_move ) != -1 ) {
-                    continue;
-                }
-
-            }
-        }
-    }
+    pass = true;
 
     function reduce_board(board) {
-        var new_board = new Array(WIDTH);
-        for(var col = 0; col < WIDTH; col++) {
-            new_board[col] = new Array(HEIGHT);
+        var new_board = [], col, row, t, total;
+        for (col = 0; col < WIDTH; col += 1) {
+            new_board[col] = [];
         }
 
-        for(var row = 0; row < HEIGHT; row++) {
-            for(var col = 0; col < WIDTH; col++) {
+        for (row = 0; row < HEIGHT; row += 1) {
+            for (col = 0; col < WIDTH; col += 1) {
                 new_board[col][row] = board[col][row];
-                if(new_board[col][row] > 0) {
-                    var t = new_board[col][row];
-                    var total = get_total_item_count(t);
-                    if( get_my_item_count(t) / total > 0.5 || get_opponent_item_count(t) / total > 0.5 ) {
+                if (new_board[col][row] > 0) {
+                    t = new_board[col][row];
+                    total = get_total_item_count(t);
+                    if (get_my_item_count(t) / total > 0.5 || get_opponent_item_count(t) / total > 0.5) {
                         new_board[col][row] = 0;
                     }
                 }
@@ -98,130 +230,133 @@ var ns = new function() {
     }
 
     function gravity(board) {
-        var mx = get_my_x();
-        var my = get_my_y();
-        var vector = { theta: 0, r: 0 };
-        for(var row = 0; row < HEIGHT; row++) {
-            for(var col = 0; col < WIDTH; col++) {
-                if(board[col][row] > 0) {
-                    var dx = col-mx;
-                    var dy = row-my;
-                    var dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy,2));
-                    if(dist==0) {
+        var force, theta, dx, dy, dist, tx, ty, d, move,
+            mx = get_my_x(), my = get_my_y(), row, col,
+            vector = {
+                theta: 0,
+                r: 0
+            };
+        for (row = 0; row < HEIGHT; row += 1) {
+            for (col = 0; col < WIDTH; col += 1) {
+                if (board[col][row] > 0) {
+                    dx = col - mx;
+                    dy = row - my;
+                    dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                    if (dist === 0) {
                         return TAKE;
                     }
 
-                    var force = 1 / Math.pow(dist,2);
-                    var theta = Math.atan2(-dy,dx);
-                    var tx = vector.r * Math.cos(vector.theta);
-                    var ty = vector.r * Math.sin(vector.theta);
+                    force = 1 / Math.pow(dist, 2);
+                    theta = Math.atan2(-dy, dx);
+                    tx = vector.r * Math.cos(vector.theta);
+                    ty = vector.r * Math.sin(vector.theta);
                     tx += force * Math.cos(theta);
                     ty += force * Math.sin(theta);
-                    vector.r = Math.sqrt(Math.pow(tx, 2) + Math.pow(ty,2));
-                    vector.theta = Math.atan2(ty,tx);
+                    vector.r = Math.sqrt(Math.pow(tx, 2) + Math.pow(ty, 2));
+                    vector.theta = Math.atan2(ty, tx);
 
                     /*
-                    if(dist < min_dist) {
-                        min_dist = dist;
-                        if(Math.abs(dx) >= Math.abs(dy)) {
-                            // Go EAST or WEST
-                            if(dx > 0) {
-                                best_move = EAST;
-                            } else {
-                                best_move = WEST;
-                            }
-                        } else {
-                            if(dy > 0) {
-                                best_move = SOUTH;
-                            } else {
-                                best_move = NORTH;
-                            }
-                        }
+                       if(dist < min_dist) {
+                       min_dist = dist;
+                       if(Math.abs(dx) >= Math.abs(dy)) {
+                    // Go EAST or WEST
+                    if(dx > 0) {
+                    best_move = EAST;
+                    } else {
+                    best_move = WEST;
+                    }
+                    } else {
+                    if(dy > 0) {
+                    best_move = SOUTH;
+                    } else {
+                    best_move = NORTH;
+                    }
+                    }
                     }
                     */
                 }
             }
         }
-        var d = vector.theta;
-        if( d <= Math.PI / 4 && d > -Math.PI / 4 ) {
-            return EAST;
-        } else if( d >= Math.PI / 4 && d <  3 * Math.PI / 4 ) {
-            return NORTH;
-        } else if( d >= 3* Math.PI / 4 || d <= -3 * Math.PI / 4 ) {
-            return WEST;
+        d = vector.theta;
+        if (d <= Math.PI / 4 && d > -Math.PI / 4) {
+            move =  EAST;
+        } else if (d >= Math.PI / 4 && d < 3 * Math.PI / 4) {
+            move =  NORTH;
+        } else if (d >= 3 * Math.PI / 4 || d <= -3 * Math.PI / 4) {
+            move =  WEST;
         } else {
-            return SOUTH;
+            move = SOUTH;
         }
 
+        return move;
+
+    }
+
+    function new_game() {
+        num_cells = WIDTH * HEIGHT;
+        num_types = get_number_of_item_types();
+    }
+
+    function make_move() {
+        var board, min_dist, best_move, fruit_goal, i;
+
+        board = get_board();
+        min_dist = 9e99;
+
+        if (pass) {
+            pass = false;
+            //return PASS;
+        }
+
+        //score(board);
+        fruit_goal = [];
+        for (i = 1; i <= get_number_of_item_types(); i += 1) {
+            fruit_goal.push({
+                fruit: i,
+                count: get_total_item_count(i)
+            });
+        }
+        fruit_goal.sort(function (a, b) {
+            return b.count - a.count;
+        });
+
+        board = reduce_board(board);
+
+
+        return gravity(board);
     }
 
 
-    function Board(init) {
-        this.fruit_grid = new Array(num_cells);
-        this.my_x = get_my_x();
-        this.my_y = get_my_y();
-        this.opp_x = get_opponent_x();
-        this.opp_y = get_opponent_y();
-    }
+    function search() {
+        var my_inv_moves = board.get_invalid_moves(MY),
+            opp_inv_moves = board.get_invalid_moves(OPP),
+            my_move, opp_move;
 
-    Board.prototype.move = function(my_move, opp_move) {
-        this.my_x += x_delta[my_move];
-        this.my_y += y_delta[my_move];
-        this.opp_x += x_delta[opp_move];
-        this.opp_y += y_delta[opp_move];
-
-        this.my_x = Math.max( this.my_x, 0 );
-        this.my_x = Math.min( this.my_x, WIDTH-1 );
-        this.my_y = Math.max( this.my_y, 0 );
-        this.my_y = Math.min( this.my_y, WIDTH-1 );
-
-        this.opp_x = Math.max( this.opp_x, 0 );
-        this.opp_x = Math.min( this.opp_x, WIDTH-1 );
-        this.opp_y = Math.max( this.opp_y, 0 );
-        this.opp_y = Math.min( this.opp_y, WIDTH-1 );
-    }
-
-    Board.prototype.get_invalid_moves = function(side) {
-        var x,y;
-        var invalid_moves = new Array();
-
-        if( side = MY )
-            {
-            x = this.my_x;
-            y = this.my_y;
+        for (my_move in std_moves) {
+            if (my_inv_moves.indexOf(my_move) !== -1) {
+                continue;
             }
-        else
-            {
-            x = this.opp_x;
-            y = this.opp_y;
+            for (opp_move in std_moves) {
+                if (opp_inv_moves.indexOf(opp_move) !== -1) {
+                    continue;
+                }
             }
-
-        if( x <= 0 ) {
-            invalid_moves.push(WEST);
-        } else if( x >= WIDTH-1 ) {
-            invalid_moves.push(EAST);
         }
-
-        if( y <= 0 ) {
-            invalid_moves.push(NORTH);
-        } else if( y >= HEIGHT-1 ) {
-            invalid_moves.push(EAST);
-        }
-
-        return invalid_moves;
     }
 
-    function xy2pos(x,y) {
-        return y*WIDTH + x;
-    }
-};
+
+    return { new_game: new_game, make_move: make_move };
+
+})();
 
 
 
 function make_move() {
+    "use strict";
     return ns.make_move();
 }
 
 function new_game() {
+    "use strict";
     ns.new_game();
 }
